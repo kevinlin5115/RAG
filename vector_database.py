67a8ab9d2b917a0afd4536213b8cc3d31ca3b1ea
Kevin Lin <kevinlin5115@gmail.com>
2025-01-03 14:54:09 -0500

    initial commit

---
+++ vector_database.py
@@ -0,0 +1,67 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from langchain_chroma import Chroma
+from embedding_model import get_embedding_model
+
+def main():
+    documents = load_documents()
+    chunks = split_documents(documents)
+    add_to_chroma(chunks)
+
+def load_documents(DATA_PATH: str="data"):
+    loader = PyPDFDirectoryLoader(DATA_PATH)
+    return loader.load()
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    return text_splitter.split_documents(documents)
+
+def add_to_chroma(chunks: list[Document], CHROMA_PATH: str="chroma", ):
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_model()
+    )
+
+    chunk_with_ids = calculate_chunk_ids(chunks)
+
+    existing_items = db.get(include=[])
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+    new_chunks = []
+    for chunk in chunk_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+    else:
+        print("No new documents to add")
+
+def calculate_chunk_ids(chunks):
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        chunk.metadata["id"] = chunk_id
+    
+    return chunks
+
+if __name__ == "__main__":
+    main()
